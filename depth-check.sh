f33a52f8f9507c44d53bcf872e480021b72d7a18
Anna Syme <AnnaSyme@users.noreply.github.com>
2020-08-17 16:50:30 +1000

    Create depth-check.sh

---
+++ depth-check.sh
@@ -0,0 +1,19 @@
+!#usr/bin/env bash
+
+#find where short reads didn't map to an assembly
+
+samtools depth -a shortmapped.bam > samout
+
+#http://www.htslib.org/doc/samtools-depth.html
+#out file is a text file with contig name, assembly position and number of reads that mapped
+#each line is a position
+
+awk '$3 == "0"' < samout > awkout
+
+#if field 3 is equal to zero
+
+wc -l awkout
+
+#num lines is number of positions with no reads that mapped
+
+
